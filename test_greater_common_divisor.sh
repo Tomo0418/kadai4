03fc667b992b93f0ec4385913327f6c3e4f850f0
z22013ts <z22013ts@aiit.ac.jp>
2022-08-02 01:37:40 +0900

    submit kadai4

--- test_greater_common_divisor.sh
+++ test_greater_common_divisor.sh
@@ -5,11 +5,9 @@
 #/* case of variable m */
 
 #m is natural number
-if [ m = 5 ];
+if [ m = 5 ];then
 	echo "OK:m is natural number."
 	exit
-else 
-	echo "NG:m is not natural number.Don't input zero,decimal points,negative numbers,chatacter strings"
 fi
 
 
