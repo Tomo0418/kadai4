9d14776dbfd3acea1d5ae652af70c7913940fdfc
z22013ts <z22013ts@aiit.ac.jp>
2022-08-03 06:34:19 +0900

    test

--- test_greater_common_divisor.sh
+++ test_greater_common_divisor.sh
@@ -5,7 +5,6 @@
 
 #/* case of variable m */
 
-#m is natural number
 if [ m = 5 ];then
 	echo "OK:m is natural number."
 	exit
@@ -33,7 +32,6 @@ else
 fi
 
 #/* case of variable n */
-n is natural number
 
 if [ n = 9 ];then
 	echo "OK:n is natural number."
@@ -62,12 +60,36 @@ fi
 
 #-----------------------------------Failure test--------------------------------------
  
-#/#--------------Case of two or more variable------------------#/
+#/###--------------case : number of variable------------------###/
 
+#/#-------------No variable-------------#/
+#/* Case of variable m */
+
+if [ m =  ];then
+	echo "OK:m is natural number."
+	exit
+
+else
+	echo "NG:m is not natural.Don't input zero,decimal points,negative numbers,character strings."
+
+fi
+
+#/* Case of variable n */
+
+if [ n =  ];then
+	echo "OK:n is natural number."
+	exit
+
+else
+	echo "NG:n is not natural.Don't input zero,decimal points,negative numbers,character strings."
+
+fi
+
+#/#--------------Case of two or more variable------------------#/
 #/* Case of variable m */
 
 
-if [ m = 125 24 ];then
+iif [ m = 125 24 ];then
 	echo "OK:m is natural number."
 	exit
 
