8947f925e0e3e30ba117f43e70d5bf9929aebce3
z22013ts <z22013ts@aiit.ac.jp>
2022-08-04 17:52:10 +0900

    test

--- test_greater_common_divisor.sh
+++ test_greater_common_divisor.sh
@@ -64,7 +64,7 @@ fi
 
 #-----------------------------------Failure test--------------------------------------
  
-#/###--------------case : number ofinputting argument to variable------------------###/
+#/###--------------case : number of inputting argument to variable------------------###/
 
 #/#-------------No argument-------------#/
 #/* Case of variable m */
@@ -91,7 +91,7 @@ else
 
 fi
 
-#/#--------------Case of inputting two more numbers to m of variable--------------#/
+#/#--------------Case of inputting two more numbers to variable--------------#/
 #/* Case of variable m */
 
 
@@ -117,3 +117,107 @@ else
 
 fi
 
+#/#--------------Case of inputting zero to variable--------------#/
+#/* Case of variable m */
+
+
+if [ m = 0 ];
+then
+	echo "OK:m is natural number."
+	exit
+
+else
+	echo "NG:m is not natural.Don't input multiple numbers,zero,decimal points,negative numbers,character strings."
+	exit1
+
+fi
+
+#/* Case of variable n */
+
+if [ n = 0 ];then
+	echo "OK:n is natural number."
+	exit
+
+else
+	echo "NG:n is not natural.Don't input multiple numbers,zero,decimal points,negative numbers,character strings."
+
+fi
+
+#/#--------------Case of inputting decimal points to variable--------------#/
+#/* Case of variable m */
+
+
+if [ m = 3.9 ];
+then
+	echo "OK:m is natural number."
+	exit
+
+else
+	echo "NG:m is not natural.Don't input multiple numbers,zero,decimal points,negative numbers,character strings."
+	exit1
+
+fi
+
+#/* Case of variable n */
+
+if [ n = 0.8 ];then
+	echo "OK:n is natural number."
+	exit
+
+else
+	echo "NG:n is not natural.Don't input multiple numbers,zero,decimal points,negative numbers,character strings."
+
+fi
+
+#/#--------------Case of inputting negative numbers to variable--------------#/
+#/* Case of variable m */
+
+
+if [ m = -13 ];
+then
+	echo "OK:m is natural number."
+	exit
+
+else
+	echo "NG:m is not natural.Don't input multiple numbers,zero,decimal points,negative numbers,character strings."
+	exit1
+
+fi
+
+#/* Case of variable n */
+
+if [ n = -104 ];then
+	echo "OK:n is natural number."
+	exit
+
+else
+	echo "NG:n is not natural.Don't input multiple numbers,zero,decimal points,negative numbers,character strings."
+
+fi
+
+#/#--------------Case of inputting decimal points to variable--------------#/
+#/* Case of variable m */
+
+
+if [ m = saitou ];
+then
+	echo "OK:m is natural number."
+	exit
+
+else
+	echo "NG:m is not natural.Don't input multiple numbers,zero,decimal points,negative numbers,character strings."
+	exit1
+
+fi
+
+#/* Case of variable n */
+
+if [ n = tomoaki ];
+then
+	echo "OK:n is natural number."
+	exit
+
+else
+	echo "NG:n is not natural.Don't input multiple numbers,zero,decimal points,negative numbers,character strings."
+
+fi
